3f709e8479fed4c2a66753b73c13761fdd9eeb51
Sergei Babrakov <s.a.babrakov@gmail.com>
2023-03-15 14:31:42 +0100

    Simple websocket app

--- src/main/java/ru/infoza/main/Main.java
+++ src/main/java/ru/infoza/main/Main.java
@@ -1,35 +1,26 @@
 package ru.infoza.main;
 
-import ru.infoza.accounts.AccountService;
-import ru.infoza.accounts.UserProfile;
+import chat.WebSocketChatServlet;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import ru.infoza.dbService.DBService;
-import ru.infoza.servlets.SessionsServlet;
-import ru.infoza.servlets.UsersServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        AccountService accountService = new AccountService();
-
-//        accountService.addNewUser(new UserProfile("admin"));
-//        accountService.addNewUser(new UserProfile("test"));
-
+        Server server = new Server(8080);
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new UsersServlet(accountService)), "/signup");
-        context.addServlet(new ServletHolder(new SessionsServlet(accountService)), "/signin");
+
+        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
 
         ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setDirectoriesListed(true);
         resource_handler.setResourceBase("public_html");
 
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
-
-        Server server = new Server(8080);
         server.setHandler(handlers);
 
         server.start();
