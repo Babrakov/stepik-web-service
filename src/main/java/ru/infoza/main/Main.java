d5f7529f45465f0ccfdd173cc4ae6ad481f48bd9
Sergei Babrakov <s.a.babrakov@gmail.com>
2023-03-19 21:05:50 +0100

    Added tests and logs

--- src/main/java/ru/infoza/main/Main.java
+++ src/main/java/ru/infoza/main/Main.java
@@ -1,30 +1,61 @@
 package ru.infoza.main;
 
-import chat.WebSocketChatServlet;
+import ru.infoza.accountServer.AccountServer;
+import ru.infoza.accountServer.AccountServerController;
+import ru.infoza.accountServer.AccountServerControllerMBean;
+import ru.infoza.accountServer.AccountServerI;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import ru.infoza.servlets.HomePageServlet;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    static final Logger logger = LogManager.getLogger(Main.class.getName());
 
-        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            logger.error("Use port as the first argument");
+            System.exit(1);
+        }
+
+        String portString = args[0];
+        int port = Integer.valueOf(portString);
+
+        logger.info("Starting at http://127.0.0.1:" + portString);
+
+        AccountServerI accountServer = new AccountServer(1);
+
+        AccountServerControllerMBean serverStatistics = new AccountServerController(accountServer);
+        MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+        ObjectName name = new ObjectName("ServerManager:type=AccountServerController");
+        mbs.registerMBean(serverStatistics, name);
+        ObjectName admin = new ObjectName("Admin:type=AccountServerController.UsersLimit");
+        mbs.registerMBean(serverStatistics, admin);
+
+        Server server = new Server(port);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new HomePageServlet(accountServer)), HomePageServlet.PAGE_URL);
 
         ResourceHandler resource_handler = new ResourceHandler();
         resource_handler.setDirectoriesListed(true);
-        resource_handler.setResourceBase("public_html");
+        resource_handler.setResourceBase("static");
 
         HandlerList handlers = new HandlerList();
         handlers.setHandlers(new Handler[]{resource_handler, context});
         server.setHandler(handlers);
 
         server.start();
-        System.out.println("Server started");
+        logger.info("Server started");
+
         server.join();
     }
 }
